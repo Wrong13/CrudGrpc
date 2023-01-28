845cf8e31e4bb7cc95bb52022f050c08a576f240
wrong <ilak71363@gmail.com>
2023-01-28 09:41:07 +0300

    Добавил поиск и добавление User

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DbAndGrpc;
+using Grpc.Core;
 using Grpc.Net.Client;
 
 using var channel = GrpcChannel.ForAddress("http://localhost:5274");
@@ -6,7 +7,21 @@ using var channel = GrpcChannel.ForAddress("http://localhost:5274");
 var client = new UserService.UserServiceClient(channel);
 
 ListReply users = await client.ListUsersAsync(new Google.Protobuf.WellKnownTypes.Empty());
+Console.WriteLine("Все юзеры");
 foreach (var user in users.Users)
 {
     Console.WriteLine($"{user.Id}. {user.Name} - {user.Age}");
 }
+Console.WriteLine("Юзер с id 2");
+UserReply userReply = await client.GetUserAsync(new GetUserRequest { Id = 2 });
+Console.WriteLine($"{userReply.Id}.{userReply.Name} - {userReply.Age}");
+
+// Добавление юзера
+try
+{
+    Console.WriteLine("Добавляю юзера Ilya с возрастом 40");
+    userReply = await client.CreateUserAsync(new CreateUserRequest { Name = "Ilya", Age = 40 });
+    Console.WriteLine($"{userReply.Id}. {userReply.Name} - {userReply.Age}");
+}
+catch (RpcException ex)
+{ Console.WriteLine(ex.Status.Detail ); }
--- DbAndGrpc/Services/UserApiService.cs
+++ DbAndGrpc/Services/UserApiService.cs
@@ -12,13 +12,37 @@ namespace DbAndGrpc.Services
         {
             this.db = db;
         }
-        // Отправка списка пользователей
+        // Отправка списка пользователей (На входе ничего,токо выход)
         public override Task<ListReply> ListUsers(Empty request, ServerCallContext context)
         {
             var listReply = new ListReply();
             var userList = db.Users.Select(x=> new UserReply { Id = x.Id, Name = x.Name,Age = x.Age }).ToList();
             listReply.Users.AddRange(userList);
             return Task.FromResult(listReply);
+        }
+
+        // Поиск по Id
+        public override async Task<UserReply> GetUser(GetUserRequest request, ServerCallContext context)
+        {
+            var user = await db.Users.FindAsync(request.Id);
+            if (user == null)
+                throw new RpcException(new Status(StatusCode.NotFound, "User не найден"));
+            UserReply userReply = new UserReply() { Id=user.Id, Name = user.Name,Age = user.Age };
+            return await Task.FromResult(userReply);
+        }
+        // Создание Юзера
+        public override async Task<UserReply> CreateUser(CreateUserRequest request, ServerCallContext context)
+        {
+            if (request == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Не введены данные"));
+            var user = new User { Name = request.Name, Age = request.Age };
+            if (await db.Users.FindAsync(user) != null)
+                throw new RpcException(new Status(StatusCode.AlreadyExists, "Юзер уже существует"));
+
+            await db.Users.AddAsync(user);
+            await db.SaveChangesAsync();
+            var reply = new UserReply() { Id = user.Id, Name = user.Name,Age = user.Age };
+            return await Task.FromResult(reply);
         }
     }
 }
